55722ad6aa2fd18996e755661ab6b169172a6d50
Matteo Balestriero <69146010+TeoBale@users.noreply.github.com>
2024-01-23 00:57:14 +0100

    Create jupyter-setup.sh

---
+++ jupyter-setup.sh
@@ -0,0 +1,20 @@
+sudo apt install ufw
+sudo ufw enable
+sudo ufw allow 22
+sudo ufw allow 8888
+sudo ufw status
+
+pip install jupyter
+jupyter notebook --generate-config
+
+if test -e /home/pi/.jupyter/jupyter_notebook_config.py; then
+  echo "c.NotebookApp.ip = '0.0.0.0'  # listen on all IPs" >> /home/pi/.jupyter/jupyter_notebook_config.py
+  echo "c.NotebookApp.token = ''  # disable authentication" >> /home/pi/.jupyter/jupyter_notebook_config.py
+  echo "c.NotebookApp.allow_origin = '*'  # allow access from anywhere" >> /home/pi/.jupyter/jupyter_notebook_config.py
+  echo "c.NotebookApp.disable_check_xsrf = True # allow cross-site requests" >> /home/pi/.jupyter/jupyter_notebook_config.py
+
+  jupyter notebook
+else
+  echo "File di configurazione di jupyter non trovato"
+fi
+
